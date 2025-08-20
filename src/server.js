ab19820c21e2f27f9eb5834e4eec6207c1fe60a1
Hieu Nguyen <hieunguyenby04@gmail.com>
2025-08-20 22:52:03 +0700

    validation

--- src/server.js
+++ src/server.js
@@ -5,8 +5,12 @@ import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
 import { APIs_V1 } from '~/routes/v1'
 const START_SERVER = () => {
+
   const app = express()
+
+  app.use(express.json())
   app.use('/v1', APIs_V1)
+
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(`Hello ${env.AUTHOR}, I am running at Host ${ env.APP_HOST } and Port ${ env.APP_PORT }`)
   })
---
+++ src/validations/boardValidation.js
@@ -0,0 +1,41 @@
+import Joi from 'joi'
+import { StatusCodes } from 'http-status-codes'
+
+const createNew = async (req, res, next) => {
+    /**
+     * Note: Mặc định thì ta không phải custom message ở phía BE vì để cho FE làm tự validate và custom message phía FE cho đẹp
+     * CE chỉ cần validate Đảm bảo dữ liệu chuẩn xác và trả về message mặc định của thư viện là được
+     * Quan trọng: việc validate dữ liệu BẮT BUỘC phải có ở BE vì đây là điểm cuối để lưu trữ dữ liệu và database
+     * Trong hệ thống điều bắt buộc là phải validate dữ liệu cả ở BE lẫn FE
+     */
+    const correctCondition = Joi.object({
+        title: Joi.string().required().min(3).max(50).trim().strict().messages({
+            'any.required': 'Title is required',
+            'string.empty': 'Title is not allowed to be empty',
+            'string.min': 'Title min 3 chars',
+            'string.max': 'Title max 50 chars',
+            'string.trim': 'Title must not have leading or trailing whitespace'
+        }),
+        description: Joi.string().required().min(3).max(256).trim().strict()
+    })
+
+    try {
+        console.log(req.body)
+        // false trường hợp có nhiều lỗi validation thì trả về tất cả các lỗi (video 52)
+        await correctCondition.validateAsync(req.body, { abortEarly: false})
+
+        // next()
+        res.status(StatusCodes.CREATED).json({ message: 'POST from Validation: API create new board' })
+    } catch (error) {
+        console.log(error);
+        res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+            errors: new Error(error).message
+        })
+    }
+
+    
+}
+
+export const boardValidation = {
+    createNew
+}
