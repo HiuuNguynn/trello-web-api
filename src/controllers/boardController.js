634bdabbdb90f8b62aa06253a09763574d7272b2
Hieu Nguyen <hieunguyenby04@gmail.com>
2025-08-24 21:53:47 +0700

    API response Error

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -2,12 +2,11 @@ import { StatusCodes } from "http-status-codes";
 
 const createNew = async (req, res, next) => {
     try {
-        console.log(req.body)
-        res.status(StatusCodes.CREATED).json({ message: 'POST from Controller: API create new board' })
-    } catch (error) {
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-            errors: error.message
+        return res.status(StatusCodes.CREATED).json({
+            message: 'Board controller is working'
         })
+    } catch (error) {
+        next(error)
     }
 }
 
