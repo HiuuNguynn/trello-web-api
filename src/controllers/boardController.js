1adb003618b8621f52f8a1d6da53c77e3d0a2632
Hieu Nguyen <hieunguyenby04@gmail.com>
2025-09-11 23:08:53 +0700

    build serivce layer

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,10 +1,10 @@
 import { StatusCodes } from "http-status-codes";
+import { boardService } from "~/services/boardService";
 
 const createNew = async (req, res, next) => {
     try {
-        return res.status(StatusCodes.CREATED).json({
-            message: 'Board controller is working'
-        })
+        const createdBoard = await boardService.createNew(req.body)
+        res.status(StatusCodes.OK).json(createdBoard)
     } catch (error) {
         next(error)
     }
