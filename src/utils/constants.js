fb5b51b5d45054499b6c67de81e782745e3e15a8
Hieu Nguyen <hieunguyenby04@gmail.com>
2025-11-08 15:44:16 +0700

    feat: add cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,4 @@
-/**
- * Updated by trunghieudev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trunghieudev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+// Only allow domains to access the API
+export const WHITELIST_DOMAINS = [
+    'http://localhost:4000',
+]
