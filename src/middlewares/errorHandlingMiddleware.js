4869bf8b7d1f07742ec4b49b390aad62a2efdb7f
Hieu Nguyen <hieunguyenby04@gmail.com>
2025-11-11 14:08:32 +0700

    remove comment

--- src/middlewares/errorHandlingMiddleware.js
+++ src/middlewares/errorHandlingMiddleware.js
@@ -1,17 +1,15 @@
 import { StatusCodes } from 'http-status-codes'
 import { env } from '~/config/environment'
 
-// Middleware xử lý lỗi tập trung trong ứng dụng Back-end NodeJS (ExpressJS)
 export const errorHandlingMiddleware = (err, req, res) => {
   if (!err.statusCode) err.statusCode = StatusCodes.INTERNAL_SERVER_ERROR
 
   const responseError = {
     statusCode: err.statusCode,
-    message: err.message || StatusCodes[err.statusCode], // Nếu lỗi mà không có message thì lấy ReasonPhrases chuẩn theo mã Status Code
+    message: err.message || StatusCodes[err.statusCode],
     stack: err.stack
   }
 
-  // Chỉ khi môi trường là DEV thì mới trả về Stack Trace để debug dễ dàng hơn, còn không thì xóa đi. (Muốn hiểu rõ hơn hãy xem video 55 trong bộ MERN Stack trên kênh Youtube: https://www.youtube.com/@trungquandev)
   if (env.BUILD_MODE !== 'dev') delete responseError.stack
 
   res.status(responseError.statusCode).json(responseError)
