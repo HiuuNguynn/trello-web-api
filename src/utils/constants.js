28b380589b879c34d40e994c7036d8a5984ea1ce
Hieu Nguyen <hieunguyenby04@gmail.com>
2025-11-15 21:44:01 +0700

    feat: add domain production to cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,9 @@
 // Only allow domains to access the API
-export const WHITELIST_DOMAINS = ['http://localhost:4000', 'http://localhost:5173']
+export const WHITELIST_DOMAINS = [
+  'http://localhost:4000',
+  'http://localhost:5173',
+  'https://trello-ykqa.onrender.com'
+]
 
 export const BOARD_TYPES = {
   PUBLIC: 'public',
