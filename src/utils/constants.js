587724d38893250f9905768443bf5e75910a53b0
Hieu Nguyen <hieunguyenby04@gmail.com>
2025-11-16 01:10:42 +0700

    fix domain CORS prod

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,8 @@ export const WHITELIST_DOMAINS = [
   'http://localhost:4000',
   'http://localhost:5173',
   'https://trello-ykqa.onrender.com',
-  'https://trello-web-project-hiuunguynns-projects.vercel.app'
+  'https://trello-web-project-hiuunguynns-projects.vercel.app',
+  'https://trello-web-project.vercel.app'
 ]
 
 export const BOARD_TYPES = {
