7596fa5264919b45a934d5ce536af4fa5aab6e04
Hieu Nguyen <hieunguyenby04@gmail.com>
2025-11-15 23:41:10 +0700

    fix deploy to prod

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,8 @@
 export const WHITELIST_DOMAINS = [
   'http://localhost:4000',
   'http://localhost:5173',
-  'https://trello-ykqa.onrender.com'
+  'https://trello-ykqa.onrender.com',
+  'https://trello-web-project-hiuunguynns-projects.vercel.app'
 ]
 
 export const BOARD_TYPES = {
